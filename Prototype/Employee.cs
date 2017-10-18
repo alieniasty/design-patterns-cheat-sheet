335a9bf9ab11f14766fcc2b050b781176af187f9
Adrian Jagodzinski <adrian.jagodzinski@cybercom.com>
2017-10-18 11:58:53 +0200

    Prototype

---
+++ Prototype/Employee.cs
@@ -0,0 +1,17 @@
+﻿namespace Prototype
+{
+    public class Employee : IEmployee
+    {
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public string EmploymentType { get; set; }
+
+        public IEmployee CloneEmployee()
+        {
+            return this.MemberwiseClone() as IEmployee;
+        }
+    }
+}
---
+++ Prototype/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Prototype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var employee = new Employee
+            {
+                Age = 22,
+                EmploymentType = "Contract",
+                Name = "Adriano Italiano"
+            };
+
+            /*
+             * Check if is of type Employee AND not null. Pattern matching
+             */
+            if (!(employee.CloneEmployee() is Employee anotherEmployee)) return; 
+            anotherEmployee.Name = "Adriano Polano";
+            anotherEmployee.Age = 32;
+            anotherEmployee.EmploymentType = "Intern";
+
+            Console.WriteLine(employee.Name);
+            Console.WriteLine(anotherEmployee.Name);
+
+            Console.ReadLine();
+        }
+    }
+}
