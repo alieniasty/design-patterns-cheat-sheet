b71c6df03e3b085cad7f8a98c007569c8135c03f
Adrian Jagodziński <adrianjago95@gmail.com>
2017-10-25 23:00:25 +0200

    Proxy

---
+++ Structural/Proxy/Proxy.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy
+{
+    /*Instead of making multiple copies of a heavy object X you make copies of a lightweight proxy P which in turn manages X and translates your calls as required.) 
+     * You are solving the problem of the client from having to manage a heavy and/or complex object.*/
+    class Proxy : Subject
+    {
+        private Subject _realSubject;
+
+        public override void Request()
+        {
+            /*Use 'lazy initialization'*/
+            if (_realSubject == null)
+            {
+                _realSubject = new Subject();
+            }
+
+            _realSubject.Request();
+        }
+    }
+}
---
+++ Structural/Proxy/Subject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy
+{
+    public class Subject : AbstractSubject
+    {
+        public override void Request()
+        {
+            Console.WriteLine("Terefere");
+        }
+    }
+}
